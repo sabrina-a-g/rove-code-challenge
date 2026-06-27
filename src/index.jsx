59d9aaaf6d5a0dc2535143f6782df6b13c765507
Sabrina Grosse <sagmag19@gmail.com>
2026-06-27 16:40:46 -0400

    lots of conversion for the react work from the bare bones

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import "./style.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Roveiq from "./App";
+
+createRoot(document.getElementById("root")).render(<Roveiq />);
